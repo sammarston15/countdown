471b434d233e2162595b214e66a9a0d2925bb1cc
SAM <sammarston15@gmail.com>
2019-10-16 16:27:27 -0600

    header work

--- src/App.js
+++ src/App.js
@@ -53,8 +53,7 @@ class App extends React.Component {
             <div className="home">
               <button onClick={this.handleStartLoad}>Click me to load</button>
               
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod repudiandae, aperiam perspiciatis vero corporis animi illum? Ab eaque necessitatibus ipsum alias, eius rem. Laboriosam voluptates molestias architecto, at quas perspiciatis!</p>
-              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nobis voluptatum eum repellat optio asperiores temporibus itaque nesciunt aperiam iste corrupti, eligendi velit excepturi fuga repudiandae incidunt facere saepe esse quisquam.</p>
+              
             </div>
           }
         </div>
