32aaafa53dd7f3b17c1b6aa581253ea26d5bd464
SAM <sammarston15@gmail.com>
2019-10-24 10:29:50 -0600

    function to add to csvData array

--- src/App.js
+++ src/App.js
@@ -3,11 +3,18 @@ import './reset.css';
 import './App.css';
 import { Switch, Route, Link } from 'react-router-dom';
 import Halloween from './components/halloween/halloween';
+import {CSVLink} from 'react-csv';
 
 class App extends React.Component {
 
   state = {
-    loading: false
+    loading: false,
+    csvData: [
+      ['FIRSTNAME', 'LASTNAME', 'EMAIL']
+    ],
+    firstname: '',
+    lastname: '', 
+    email: ''
   }
 
 
@@ -35,7 +42,28 @@ class App extends React.Component {
     }, 3000)
   }
 
+  handleChange = e => this.setState({[e.target.name]: e.target.value})
+
+  handleSave = () => {
+    this.state.csvData.push([this.state.firstname, this.state.lastname, this.state.email])
+    this.setState({
+      firstname: '',
+      lastname: '', 
+      email: ''
+    })
+  }
+
+  onKeyPress = (e) => {
+    if (e.which === 13) {
+      this.handleSave();
+    }
+  }
+
   render() {
+    
+    const {firstname, lastname, email} = this.state
+    console.log(this.state)
+
     return (
       <div className="App">
         <header className='header'>
@@ -57,14 +85,20 @@ class App extends React.Component {
                   this.handleLoading()
                   :
                   <div className="home">
+                    <div style={{fontSize: '50px', color: 'whitesmoke', maxWidth: '400px', height: 'auto', textAlign: 'center'}}>Hello all, enter info to put in the csv file</div>
                     <button onClick={this.handleStartLoad}>Click me to load</button>
-                    
-                    
+                    <input name="firstname" value={firstname} placeholder='firstname' onChange={this.handleChange} />
+                    <input name="lastname" value={lastname} placeholder='lastname' onChange={this.handleChange} />
+                    <input name="email" value={email} placeholder='email' onChange={this.handleChange} onKeyPress={this.onKeyPress}/>
+                    <button onClick={this.handleSave}>submit to csv</button>
+                    <CSVLink data={this.state.csvData} target='_blank'>
+                      <button>download me as .csv file</button>
+                    </CSVLink>
                   </div>
                 }
               </Route>
               <Route path='/halloween'>
-                <Halloween timeTillDate="10 26 2019, 6:00 am" timeFormat="MM DD YYYY, h:mm a" />
+                <Halloween timeTillDate="10 31 2019, 12:00 am" timeFormat="MM DD YYYY, h:mm a" />
               </Route>
             </Switch>
         </div>
