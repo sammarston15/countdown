4d8edef815995a5e03fed9d442bb4b5d77a3f05e
SAM <sammarston15@gmail.com>
2019-12-20 15:15:50 -0700

    movie

--- src/components/movies/movies.js
+++ src/components/movies/movies.js
@@ -5,38 +5,45 @@ import axios from 'axios';
 class Movies extends Component {
 
     state = {
-        movies: {}
+        movies: []
     }
 
     componentDidMount() {
-        axios.get('https://api.themoviedb.org/3/discover/movie?api_key=e3b88ea03f5f916f87b2657d0a7a6ebf&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1')
-            .then(response => {
-                console.log('got it')
-                this.setState({movies: response.data})
-                console.log(this.state.movies)
-            })
+        let loadMovies = this.getMovies();
     }
 
 
     getMovies = () => {
-        axios.get('https://api.themoviedb.org/3/movie/550?api_key=e3b88ea03f5f916f87b2657d0a7a6ebf')
+        axios.get('https://api.themoviedb.org/3/discover/movie?api_key=e3b88ea03f5f916f87b2657d0a7a6ebf&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1')
+            .then(response => {
+                this.setState({movies: response.data})
+                console.log(this.state.movies)
+                
+            })
     }
 
 
     render() {
 
         // let moviesMap = this.state.movies.map((movie, i) => {
+            
+        //     console.log(movie[i])
         //     return (
-        //         div.movie
+        //         <div className="movie-card">
+        //             Title: {movie[i]}
+        //         </div>
         //     )
         // })
+        // console.log(this.state.movies)
         return (
             <div className="movies-body">
                 <div className="search-bar-container">
                     <input type="text" placeholder="Search"/>
+                    <button>SEARCH</button>
                 </div>
                 <div className="results-container">
                     {/* {this.state.moviesMap} */}
+                    hello there
                 </div>
             </div>
         );
