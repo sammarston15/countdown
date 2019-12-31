20ac5432d8a5978d4dd9ad17c51fc9db64066e63
SAM <sammarston15@gmail.com>
2019-12-31 09:55:37 -0700

    changes

--- src/components/movies/movies.js
+++ src/components/movies/movies.js
@@ -16,24 +16,33 @@ class Movies extends Component {
     getMovies = () => {
         axios.get('https://api.themoviedb.org/3/discover/movie?api_key=e3b88ea03f5f916f87b2657d0a7a6ebf&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1')
             .then(response => {
-                this.setState({movies: response.data})
+                this.setState({movies: response.data.results})
                 console.log(this.state.movies)
                 
+            })
+    }
+    getMoviePoster = (movie) => {
+        axios.get(`https://image.tmdb.org/t/p/w500${movie.poster_path}`)
+            .then(results => {
+                console.log('got the image')
+                this.setState({posterImage: results.data})
+                return this.state.posterImage;
             })
     }
 
 
     render() {
 
-        // let moviesMap = this.state.movies.map((movie, i) => {
+        let moviesMap = this.state.movies.map((movie, i) => {
             
-        //     console.log(movie[i])
-        //     return (
-        //         <div className="movie-card">
-        //             Title: {movie[i]}
-        //         </div>
-        //     )
-        // })
+            
+            return (
+                <div className="movie-card">
+                    Title: {movie.title}
+                    {this.getMoviePoster(movie)}
+                </div>
+            )
+        })
         // console.log(this.state.movies)
         return (
             <div className="movies-body">
@@ -42,8 +51,7 @@ class Movies extends Component {
                     <button>SEARCH</button>
                 </div>
                 <div className="results-container">
-                    {/* {this.state.moviesMap} */}
-                    hello there
+                    {moviesMap}
                 </div>
             </div>
         );
